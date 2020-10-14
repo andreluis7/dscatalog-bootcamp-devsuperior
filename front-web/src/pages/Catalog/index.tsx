072c256761a5732d432d9a29d4b45a41506139ad
Andre Luis <adrluis7@gmail.com>
2020-10-13 22:11:01 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const params = {
@@ -14,8 +16,12 @@ const Catalog = () => {
             linesPerPage: 12
         }
 
-        makeRequest({url: '/products', params})
-        .then(response => setProductsResponse(response.data));
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            });
     }, []);
 
     return (<div className="catalog-container">
@@ -23,11 +29,13 @@ const Catalog = () => {
             Catalogo de produtos
     </h1>
         <div className="catalog-products">
-            {productsResponse?.content.map(product => (
-                <Link to= {`/products/${product.id}`} key={product.id}>
-                    <ProductCard product= { product } />
-                </Link>
-            ))}
+            {isLoading ? <ProductCardLoader /> : (
+                productsResponse?.content.map(product => (
+                    <Link to={`/products/${product.id}`} key={product.id}>
+                        <ProductCard product={product} />
+                    </Link>
+                ))
+            )}
         </div>
     </div>);
 };
